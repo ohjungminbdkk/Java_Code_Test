cc2c5b2efcf8c8c1decf1fba9b819f5ad6711d93
bdkk0 <bdkk0@192.168.2.107>
2020-06-11 23:41:07 +0900

    Lv1

---
+++ src/lv1/Army.java
@@ -0,0 +1,11 @@
+package lv1;
+
+public class Army {
+
+	public static void main(String[] args) {
+		System.out.println("강한친구 대한육군");
+		System.out.println("강한친구 대한육군");
+		
+	}
+
+}
---
+++ src/lv1/HelloWolrd.java
@@ -0,0 +1,9 @@
+package lv1;
+
+public class HelloWolrd {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
